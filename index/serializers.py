8eee426f04abf8acee5f7ed7f26ee56dd21f899f
maizihao <pumpkin_mak@163.com>
2021-11-08 21:56:19 +0800

    无

--- index/serializers.py
+++ index/serializers.py
@@ -30,3 +30,27 @@ class VocationSerializer(serializers.ModelSerializer):
         model = Vocation
         # fields = '__all__'
         fields = ('id', 'job', 'title', 'payment', 'name')
+
+    def create(self, validated_data):
+        #
+        name = validated_data.get('name', '')
+        id = validated_data('id', 0)
+        p = PersonInfo.objects.filter(id=id).first()
+        if not p:
+            p = PersonInfo.objects.create(**name)
+        data = validated_data
+        data['name'] = p
+        v = Vocation.objects.create(**data)
+        return v
+
+    def updata(self, operation, validated_data):
+        name = validated_data.get('name', '')
+        id = validated_data('id', 0)
+        p = PersonInfo.objects.filter(id=id).first()
+        if p:
+            PersonInfo.objects.filter(id=id).update(**name)
+            data = validated_data
+            data['name'] = p
+            id = validated_data.get('id', '')
+            v = Vocation.objects.filter(id=id).update(**data)
+            return v
