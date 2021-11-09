10191cb21f960d5edd3b5bc7d946d4e8a27bc7c8
Lin-JP <lking889@hotmail.com>
2021-11-09 16:47:14 +0800

    无

--- DRF_study/settings.py
+++ DRF_study/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 MIDDLEWARE = [
