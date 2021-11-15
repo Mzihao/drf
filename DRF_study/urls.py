b363b45a6ad812c39c455df5507f869713463599
Lin-JP <lking889@hotmail.com>
2021-11-15 16:11:33 +0800

    修改一些重复代码

--- DRF_study/urls.py
+++ DRF_study/urls.py
@@ -19,8 +19,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('api', include('index.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('pumpkin/', admin.site.urls),
+    path('', include('index.urls')),
 ]
